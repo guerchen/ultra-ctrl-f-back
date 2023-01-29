555fb2c7617ce1eb49451ea010b7682870090842
guerchen <ariel.guerchen.10@gmail.com>
2023-01-29 18:09:10 -0300

    chore: changed from pymongo to motor, make it work

--- app/main.py
+++ app/main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from lib2to3.pytree import Base
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
+from pydantic import BaseModel
+from typing import List
+from app.mongo import db
 
 app = FastAPI()
 
+class PageData(BaseModel):
+    url: str
+    images: List[str]
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/")
+async def save_images(pageData: PageData):
+    new_site_data = await db.site_images.insert_one(jsonable_encoder(pageData))
+    created_document = await db.site_images.find_one({"_id": str(new_site_data.inserted_id)})
+    print(created_document)
+    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_document)
--- app/mongo.py
+++ app/mongo.py
@@ -1,8 +1,9 @@
-import pymongo
+import motor.motor_asyncio
 
-client = pymongo.MongoClient(
+client = motor.motor_asyncio.AsyncIOMotorClient(
     "mongodb+srv://admin:x0FZl46PiWffq4eV@ultra-ctrl-f.ecday84.mongodb.net/?retryWrites=true&w=majority"
     )
+
 db = client.site_images
 
 print(db.SiteImages.find_one())
