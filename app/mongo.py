2cbc1cb8acef70911e167d5d3d59e3c7de70b116
guerchen <ariel.guerchen.10@gmail.com>
2023-01-29 21:31:34 -0300

    fix: resolved security vulnerability

--- app/mongo.py
+++ app/mongo.py
@@ -1,9 +1,9 @@
 from typing import Collection
 import motor.motor_asyncio
-import asyncio
+from app.env import mongo 
 
 client = motor.motor_asyncio.AsyncIOMotorClient(
-    ""
+    f"mongodb+srv://{mongo['username']}:{mongo['password']}@ultra-ctrl-f.ecday84.mongodb.net/?retryWrites=true&w=majority"
     )
 
 db = client.site_images
